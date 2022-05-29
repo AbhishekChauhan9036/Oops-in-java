12dacc6ed5d555b8e081ebdfeaec46c8f4b5025c
Abhi <abhishekchauhan9036@gmail.com>
2022-05-29 06:11:50 +0530

    commit message

---
+++ Objectss.java
@@ -0,0 +1,11 @@
+class One{
+    public void printName(){
+        System.out.println("Hello World");
+    }
+}
+public class Objectss{
+    public static void main(String[] args) {
+        One o1=new One();
+        o1.printName();
+    }
+}
